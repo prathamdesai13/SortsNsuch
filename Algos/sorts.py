8e26d89d4b74db94f2bcbd0aef15d6f76f9205a4
prathamdesai <prathamdesai13@gmail.com>
2018-11-04 01:05:27 -0500

    added insertion yeet

--- Algos/sorts.py
+++ Algos/sorts.py
@@ -8,12 +8,23 @@ def swap(arr, i, j):
 def bubble_sort(arr):
     
     for i in range(len(arr)):
+        print(arr)
         for j in range(i):
             if arr[i]  < arr[j]:
                 swap(arr, i, j)
     return arr
 
-def mergesort(arr):
+def insertion_sort(arr):
+    # basically bubble sort
+    sorted = [x for x in arr]
+    for i in range(1, len(arr)):
+        print(sorted)
+        for j in range(i):
+            if (arr[i] < sorted[j]):
+                swap(sorted, i, j)
+    return sorted
+
+def merge_sort(arr):
     if len(arr) == 1:
         return arr
     if len(arr) == 2:
--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from Algos import sorts
 
 if __name__ == '__main__':
 	
-	arr = [8, 5, 4, 4, 3, 2, 1]
-	print(sorts.mergesort(arr))
+	arr = [12, 11, 13, 5, 6]
+	print(sorts.bubble_sort(arr))
 

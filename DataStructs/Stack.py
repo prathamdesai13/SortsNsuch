c62c1f7c9ad3fc72416a64924cf0838fa7ac2512
Pratham Desai <prathamdesai13@gmail.com>
2018-12-21 12:00:23 -0500

    q

--- DataStructs/Stack.py
+++ DataStructs/Stack.py
@@ -6,16 +6,23 @@ class Stack:
         self.current_index = 0
 
     def push(self, x):
-        self.container[self.current_index] = x
-        self.current_index += 1
-
+        if self.size() < 10:
+            self.container[self.current_index] = x
+            self.current_index += 1
+        else:
+            print("Stack is full")
     def pop(self):
         if not self.isEmpty():
             self.current_index -= 1
             popped_element = self.container[self.current_index]
             self.container[self.current_index] = None
             return popped_element
-        print("Stack is empty")
+        print("Stack is empty, cannot pop")
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.container[self.current_index - 1]
+        print("Stack is empty, cannot peek")
 
     def size(self):
         return self.current_index
@@ -25,5 +32,5 @@ class Stack:
             return False
         return True
 
-    def display(self):
-        print(self.container)
+    def __str__(self):
+        return self.container.__str__()
